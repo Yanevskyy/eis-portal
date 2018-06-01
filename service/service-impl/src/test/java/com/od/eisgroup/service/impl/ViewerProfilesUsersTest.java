e406d1a4f74e5f87b8613c58f7ee13d0fa68c73d
Y <igor2000@inbox.ru>
2018-06-01 08:39:18 +0300

    new modules Domain

---
+++ service/service-impl/src/test/java/com/od/eisgroup/service/impl/ViewerProfilesUsersTest.java
@@ -0,0 +1,19 @@
+package com.od.eisgroup.service.impl;
+
+import org.junit.Test;
+
+/**
+ * @author Yanevskyy Igor i.yanevskyy@gmail.com.
+ */
+public class ViewerProfilesUsersTest {
+    @Test
+    public void displayFoundUsers() throws Exception {
+
+    }
+
+    @Test
+    public void displayAllUsers() throws Exception {
+
+    }
+
+}
